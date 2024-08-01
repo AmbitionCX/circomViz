36455e40beda5f2f36ddc641d3ccd02272ba9f9c
ZongKun Jiang <23307130019@m.fudan.edu.cn>
2024-08-01 22:30:33 +0800

    fix: ManacoEditor v-model bug fix

--- Frontend/src/components/CircomCode.vue
+++ Frontend/src/components/CircomCode.vue
@@ -4,7 +4,7 @@
       <h2 class="text-base font-bold mb-2">Circom Code Input Area</h2>
     </div>
     <div class="grow border">
-      <MonacoEditor v-model="code" class="w-full h-full" />
+      <MonacoEditor v-model="circuitStore.code" class="w-full h-full" />
     </div>
     <div>
       <el-button @click="generateCircuit" type="primary" class="mt-2">Generate Circuit</el-button>
@@ -15,30 +15,26 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-
 import MonacoEditor from '@/components/MonacoEditor.vue';
-import { generate_circuit, generate_circuit_request, generate_circuit_response } from '@/apis/index.ts'
+import { generate_circuit, generate_circuit_request, generate_circuit_response } from '@/apis/index.ts';
 import { useCircuitStore } from '@/stores/circuit';
 
-const code = ref<string>('');
 const circuitStore = useCircuitStore();
 const error = ref<string>('');
 
 const generateCircuit = async () => {
-  if (!code.value) {
+  if (!circuitStore.code) {
     console.log("Empty code");
   } else {
-    
     let data: generate_circuit_request = {
-      code: code.value
-    }
+      code: circuitStore.code,
+    };
 
     generate_circuit(data).then((response: generate_circuit_response) => {
       console.log("Circom code compiled:", response.compilationId);
-
       circuitStore.setCompilationId(response.compilationId);
     }).catch((err: any) => {
-      console.log(error)
+      console.log(error);
       error.value = err.response?.data?.error || 'Error generating circuit';
     });
   }
